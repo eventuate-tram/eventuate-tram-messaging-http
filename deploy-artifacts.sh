6f6671ae42abf0da2b285a43b8680e53b15a9b4d
Chris Richardson <chris@chrisrichardson.net>
2019-11-30 15:00:59 -0800

    Added missing gradle assemble

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -23,7 +23,7 @@ VERSION=$BRANCH
 $PREFIX ./gradlew -P version=${VERSION} \
   -P bintrayRepoType=${BINTRAY_REPO_TYPE} \
   -P deployUrl=https://dl.bintray.com/eventuateio-oss/eventuate-maven-${BINTRAY_REPO_TYPE} \
-  testClasses bintrayUpload
+  testClasses assemble bintrayUpload
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
