1c05d787ea7aa4ccf36bd80f39898c0a1256ff7d
Chris Richardson <chris@chrisrichardson.net>
2021-03-19 11:14:41 -0700

    Run bootRepackage before publish to create service JAR

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -2,6 +2,6 @@
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-./gradlew assemble publishEventuateArtifacts
+./gradlew publishEventuateArtifacts
 
 ./gradlew publishEventuateDockerImages
