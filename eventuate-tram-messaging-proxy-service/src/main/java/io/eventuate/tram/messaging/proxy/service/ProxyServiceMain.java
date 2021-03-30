55ea51509fd25b94ef8207a48ab3d3e043175817
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-03-30 18:34:25 +0300

    Fixed used annotation for main configuration. Optimized circleci config.

--- eventuate-tram-messaging-proxy-service/src/main/java/io/eventuate/tram/messaging/proxy/service/ProxyServiceMain.java
+++ eventuate-tram-messaging-proxy-service/src/main/java/io/eventuate/tram/messaging/proxy/service/ProxyServiceMain.java
@@ -1,11 +1,12 @@
 package io.eventuate.tram.messaging.proxy.service;
 
+import io.eventuate.tram.messaging.proxy.consumer.EventuateMessageSubscriberConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(ProxyConfiguration.class)
+@Import(EventuateMessageSubscriberConfiguration.class)
 public class ProxyServiceMain {
   public static void main(String[] args) {
     SpringApplication.run(ProxyServiceMain.class, args);
